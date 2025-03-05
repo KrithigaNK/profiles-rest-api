043725a2c4328b438c66432b517c621f9d3ee79d
KrithigaNN <82623385+KrithigaNN@users.noreply.github.com>
2025-03-05 13:29:15 -0600

    created django project and app

---
+++ myenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\enknk\documents\learning\dataengineering\githubprofile\python\profiles-rest-api\myenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
